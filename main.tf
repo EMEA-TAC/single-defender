de9e4f2a0b204059c9016cf8227a9c426c268f34
Mastre21Palo <134486079+Mastre21Palo@users.noreply.github.com>
2024-03-07 16:10:22 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,32 +1,45 @@
+variable "token" {
+  description = "Authorization token for API access"
+  type        = string
+  sensitive   = true
+}
+
 provider "google" {
-  project = "emea-tac-cloud-and-compute"
-  credentials = "${file("emea-tac-cloud-and-compute-f84a3722b39d.json")}"
-  region = "us-central1"
-  zone = "us-central1-a"
+  project     = "emea-tac-cloud-and-compute"
+  credentials = file("emea-tac-cloud-and-compute-f84a3722b39d.json")
+  region      = "us-central1"
+  zone        = "us-central1-a"
 }
+
 resource "google_compute_instance" "my_instance" {
-name = "kuba-defender"
-machine_type = "e2-medium"
-zone = "us-central1-a"
-allow_stopping_for_update = true
+  name         = "kuba-defender-linux"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  allow_stopping_for_update = true
 
-boot_disk {
-  initialize_params {
-    image = "ubuntu-1804-bionic-v20220810"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20220810"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Pusta konfiguracja IP
+    }
+  }
+
+  metadata = {
+    startup-script = <<-EOF
+      #!/bin/bash
+
+      DEFENDER_INSTALL_SCRIPT="https://europe-west3.cloud.twistlock.com/eu-2-143570911/api/v1/scripts/defender.sh"
+      DEFENDER_CONTROLLER_HOST="europe-west3.cloud.twistlock.com"
+      TOKEN="${var.token}"
+
+      # Download and install Defender
+      curl -sSL --header "Authorization: Bearer $TOKEN" -X POST "$DEFENDER_INSTALL_SCRIPT" | sudo bash -s -- -c "$DEFENDER_CONTROLLER_HOST" -v --install-host
+    EOF
   }
 }
-
-network_interface {
-  network = "default"
-  access_config {
-    //
-  }
-}
-
-tags = ["prisma-defender"]
-
-  metadata_startup_script = file("startup-script.sh")  # Provide your startup script file
-}
-
-
-
